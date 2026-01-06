deb94368b31a16c41f03d5e913238ff696d7d027
clngo <clngo654@gmail.com>
2026-01-05 16:24:31 -0800

    hyperparams

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,8 @@ import numpy as np
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
 from src.exception import CustomException
 
 def save_object(file_path, obj):
@@ -18,13 +20,21 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
 
         for idx in range(len(list(models))):
             model = list(models.values())[idx]
+            
+            param = params[list(models.keys())[idx]]
+
+            gs = GridSearchCV(model, param, cv=3) # why cv=3?
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train) # train model
+
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
             
